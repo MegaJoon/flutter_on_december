536bd3393ff298a6d887a9d4e62885a8ffeeaa6d
MegaJoon <54565616+MegaJoon@users.noreply.github.com>
2019-12-03 22:37:20 +0900

    food delivery app - 12.03
    food delivery app - 12.03

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_on_december/1202/blackberry_app.dart';
+import 'package:flutter_on_december/1203/airbnb_menu.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,7 +8,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: BlackberryApp(),
+      home: BlackberryApp(),  // 12-02
+//      home: AirbnbMenuApp(),  // 12-03
     );
   }
 }
