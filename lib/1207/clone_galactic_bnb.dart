f7778eeba8fcea452f012435beb8fb6afd461128
MegaJoon <54565616+MegaJoon@users.noreply.github.com>
2019-12-07 13:32:50 +0900

    galactic bnb app clone - 12.07
    galactic bnb app clone - 12.07

---
+++ lib/1207/clone_galactic_bnb.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+// https://dribbble.com/shots/6448509-Galacticbnb-App
+
+class CloneGalacticBnb extends StatefulWidget {
+  @override
+  _CloneGalacticBnbState createState() => _CloneGalacticBnbState();
+}
+
+class _CloneGalacticBnbState extends State<CloneGalacticBnb> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Placeholder(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:flutter_on_december/1204/american_express.dart';
 import 'package:flutter_on_december/1205/airbnb_app.dart';
 import 'package:flutter_on_december/1205/airbnb_concept.dart';
 import 'package:flutter_on_december/1206/galactic_bnb.dart';
+import 'package:flutter_on_december/1207/clone_galactic_bnb.dart';
 
 void main() => runApp(MyApp());
 
@@ -17,7 +18,8 @@ class MyApp extends StatelessWidget {
 //      home: AmericanExpress(),  // 12-04
 //      home: AirbnbConcept(),  // 12-05
 //      home: AirbnbApp(),  // 12-05
-      home: GalacticBnb(),  // 12-06
+//      home: GalacticBnb(),  // 12-06
+      home: CloneGalacticBnb(),  // 12-07
     );
   }
 }
