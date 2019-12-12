e084de04a3504d9fefaf059f80b6031dab351870
MegaJoon <54565616+MegaJoon@users.noreply.github.com>
2019-12-12 23:00:03 +0900

    set list<characterlist>
    set list<characterlist>

---
+++ lib/1212/character_list.dart
@@ -0,0 +1,11 @@
+class CharacterList {
+  String image;
+  String title;
+
+  CharacterList({this.image, this.title});
+}
+
+List<CharacterList> characterList = [
+  CharacterList(image: "assets/1212/image.png", title: "Kavin"),
+  CharacterList(image: "assets/1212/image1.png", title: "Agnes"),
+];
