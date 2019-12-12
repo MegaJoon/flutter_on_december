8fab3d457d3ebad2c7c90f1800c7ef6b419c2ec3
MegaJoon <54565616+MegaJoon@users.noreply.github.com>
2019-12-12 22:55:22 +0900

    build pageview
    build pageview

--- lib/1212/movie_character_ui.dart
+++ lib/1212/movie_character_ui.dart
@@ -8,10 +8,155 @@ class MovieCharachterUI extends StatefulWidget {
 }
 
 class _MovieCharachterUIState extends State<MovieCharachterUI> {
+  // double
+  double padding = 16.0;
+
+  // String
+  String title = "Despicable Me";
+  String title1 = "Characters";
+
+  // pageview controller
+  PageController _pageController;
+
+  @override
+  void initState() {
+    _pageController = PageController(initialPage: 0);
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Placeholder(),
+      body: SafeArea(
+        top: true,
+        left: true,
+        right: true,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            // appbar
+            Container(
+              height: 32.0,
+              child: Row(
+                children: <Widget>[
+                  // left icon
+                  IconButton(
+                    onPressed: (){},
+                    icon: Icon(Icons.arrow_back_ios, size: 24.0, color: Colors.black),
+                  ),
+
+                  Spacer(),
+
+                  // right icon
+                  IconButton(
+                    onPressed: (){},
+                    icon: Icon(Icons.search, size: 24.0, color: Colors.black),
+                  ),
+                ],
+              ),
+            ),
+
+            // title
+            Container(
+              padding: EdgeInsets.only(top: padding, left: padding, right: padding),
+              height: 100.0,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  // text : title
+                  Text(title,
+                    style: TextStyle(
+                      fontSize: 32.0, color: Colors.black, fontWeight: FontWeight.bold,
+                    ),
+                  ),
+
+                  Text(title1,
+                    style: TextStyle(
+                      fontSize: 28.0, color: Colors.black, fontWeight: FontWeight.w300,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+            // pageview
+            Flexible(
+              fit: FlexFit.tight,
+              child: Container(
+                child: PageView.builder(
+                    scrollDirection: Axis.horizontal,
+                    controller: _pageController,
+                    itemBuilder: (context, index){
+                      return Container(
+                        padding: EdgeInsets.all(padding),
+                        child: Stack(
+                          children: <Widget>[
+                            // background color
+                            Positioned.fill(
+                                child: ClipPath(
+                                  clipper: MyClipper(),
+                                  child: Container(
+                                    decoration: BoxDecoration(
+                                      gradient: LinearGradient(
+                                        colors: [Colors.red, Colors.red.withOpacity(0.80)],
+                                        begin: Alignment.centerLeft,
+                                        end: Alignment.centerRight,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                            ),
+                          ],
+                        ),
+                      );
+                    }),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
+
+// background color container clipper
+class MyClipper extends CustomClipper<Path> {
+  double radiusCorner = 16.0;
+  @override
+  Path getClip(Size size) {
+    var path = Path()
+      ..lineTo(0.0, size.height * 0.40 + radiusCorner)
+      ..lineTo(0.0, size.height - radiusCorner)
+      ..quadraticBezierTo(radiusCorner * 0.25, size.height - radiusCorner * 0.25, radiusCorner, size.height)
+      ..lineTo(size.width - radiusCorner, size.height)
+      ..quadraticBezierTo(size.width - radiusCorner * 0.25, size.height - radiusCorner * 0.25, size.width, size.height - radiusCorner)
+      ..lineTo(size.width, size.height * 0.25 + radiusCorner)
+      ..quadraticBezierTo(
+          size.width - radiusCorner * 0.25, size.height * 0.25 + radiusCorner * 0.25,
+          size.width - radiusCorner, size.height * 0.25 + radiusCorner * 0.20)
+
+      ..lineTo(radiusCorner, size.height * 0.40)
+
+      ..quadraticBezierTo(
+          radiusCorner * 0.25, size.height * 0.40 + radiusCorner * 0.25,
+          0.0, size.height * 0.40 + radiusCorner)
+      ..close();
+
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) => false;
+}
+
+
+
+
+
+
+
+
+
+
+
+
