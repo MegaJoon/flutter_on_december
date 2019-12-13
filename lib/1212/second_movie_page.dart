f2aa6efdd6413ee38199ae14261936f561872421
MegaJoon <54565616+MegaJoon@users.noreply.github.com>
2019-12-13 21:08:44 +0900

    movie character app - 12.13
    movie character app - 12.13

--- lib/1212/second_movie_page.dart
+++ lib/1212/second_movie_page.dart
@@ -17,8 +17,12 @@ class _SecondMoviePageState extends State<SecondMoviePage> {
   double radius = 24.0;
   double padding = 16.0;
 
+  double positionY = 485.0;
+
   @override
   Widget build(BuildContext context) {
+    final screenHeight = MediaQuery.of(context).size.height;
+
     return Scaffold(
       body: SafeArea(
         top: true,
@@ -117,8 +121,105 @@ class _SecondMoviePageState extends State<SecondMoviePage> {
             ),
 
             // bottom sheet
-            Positioned(
-              
+            AnimatedPositioned(
+              duration: Duration(milliseconds: 100),
+              curve: Curves.fastOutSlowIn,
+              top: positionY,
+              left: 0,
+              right: 0,
+              child: GestureDetector(
+                onTap: (){
+                  setState(() {
+                    // state == minimun
+                    positionY = 600.0;
+                  });
+                },
+
+                onVerticalDragStart: (DragStartDetails dragStartDetails){},
+                onVerticalDragUpdate: (DragUpdateDetails dragUpdateDetails){
+                  setState(() {
+                    positionY += dragUpdateDetails.delta.dy;
+                    print("positionY = $positionY");
+
+                    // minimun == 600.0
+                    if(positionY > 600.0) positionY = 600.0;
+
+                    // halt state == 485.0;
+
+                    // full state == 326.0;
+                    if(positionY < 326.0) positionY = 326.0;
+                  });
+                },
+
+                onVerticalDragEnd: (DragEndDetails  dragEndDetails){
+                  setState(() {
+                    if(positionY < (485.0 - 326.0) /2 + 326.0) positionY = 326.0;
+                    else positionY = 485.0;
+                  });
+                },
+
+
+                child: Container(
+                  padding: EdgeInsets.all(padding),
+                  height: screenHeight * 0.50,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(radius),
+                      topRight: Radius.circular(radius),
+                    ),
+                    color: Colors.white,
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      // title
+                      Text("Clips(16)",
+                        style: TextStyle(
+                          fontSize: 20.0, color: Colors.black, fontWeight: FontWeight.w600,
+                        ),
+                      ),
+
+                      // gridview
+                      Container(
+                        height: 300.0 - 13.3,
+                        child: GridView.builder(
+                          padding: EdgeInsets.only(top: padding + 4.0),
+                            physics: NeverScrollableScrollPhysics(),
+                            shrinkWrap: true,
+                            itemCount: 4,
+                            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                              crossAxisCount: 2,
+                              crossAxisSpacing: padding,
+                              mainAxisSpacing: padding,
+                              childAspectRatio: 1.5,
+                            ),
+                            itemBuilder: (context, index){
+                              return Container(
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(radius /2),
+                                  gradient: LinearGradient(
+                                    colors: [
+                                      Colors.blue.withOpacity(0.50),
+                                      Colors.blue.withOpacity(0.20),
+                                    ],
+                                    begin: Alignment.topCenter,
+                                    end: Alignment.bottomCenter,
+                                  ),
+                                  boxShadow: [BoxShadow(
+                                    color: Colors.black12,
+                                    spreadRadius: 1,
+                                    blurRadius: 1,
+                                  )],
+                                ),
+                                child: Image.asset(characterList[widget.index].image, fit: BoxFit.contain),
+                              );
+                            },
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
             ),
           ],
         ),
